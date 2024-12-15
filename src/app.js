72c308f9b4d04f326ac5953a3085838d3691e3cd
Ananiasnt <netimcunha@gmail.com>
2024-12-15 15:49:38 -0300

    criação do Sign-up sem o bcrypt

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
-import express from "express";
+import express, { json } from "express";
 import { MongoClient } from "mongodb";
+import cors from "cors";
+import Joi from "joi";
 
 const app=express();
+app.use(json());
+app.use(cors());
 let db;
+const userSchema= Joi.object({
+    name:Joi.string().required(),
+    email: Joi.string().email().required(),
+	password:Joi.string().min(6).required(),
+    confirm:Joi.string().required(),
+})
 const mongoClient=new MongoClient("mongodb://localhost:27017")
 
 mongoClient.connect()
@@ -12,13 +22,45 @@ mongoClient.connect()
   })
   .catch((err) => console.log(err.message));
 
+ app.post("/sign-up",async (req,res)=>{
+    const user=req.body;
+    const { error }=userSchema.validate(user,{abortEarly:false})
+
+    if (error) {
+        return res.status(422).send({
+          details: error.details.map((detail) => detail.message),
+        });
+      }
+    try{
+    if (user.password!==user.confirm){
+        return res.status(422).send("senha e confirmação de senha são diferentes");
+    }
+    const emailCadastrado= await db.collection("usuarios").findOne({email: user.email})
+    if(emailCadastrado){
+        return res.status(409).send('Este email já está em uso' );
+    }
+    
+        const newUser = {
+            name: user.name,
+            email: user.email,
+            password: user.password,
+          };
+          await db.collection("usuarios").insertOne(newUser);
+          res.status(201).send("Usuário cadastrado com sucesso!" );
+
+    }catch (error) {
+      console.error(error);
+      res.status(500).send("Erro interno do servidor. Tente novamente mais tarde.");
+    }
+    })
+  
+  app.post("/sign-in",async (req,res)=>{
+    //logar usuario
+  })
+ 
+
+  
 app.listen(5000,()=>{
     console.log("rodando liso")
 });
-
-
-
-
-
-
 
