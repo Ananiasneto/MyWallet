cb1829d1e52423cf274b41f0e6272a7d1251f91e
Ananiasnt <netimcunha@gmail.com>
2024-12-19 13:18:01 -0300

    paginação concluida e criação do put de transiçoes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express, { json } from "express";
-import { MongoClient} from "mongodb";
+import { MongoClient , ObjectId} from "mongodb";
 import cors from "cors";
 import Joi from "joi";
 import bcrypt from "bcrypt";
@@ -113,11 +113,11 @@ app.post("/transactions",async(req,res)=>{
     const { authorization } = req.headers;
     const token = authorization?.replace("Bearer", "").trim();
     if(!token){
-      return res.sendStatus(401); 
+      return res.status(401).send("token invalido"); 
     }
   const sessao=await db.collection("sessoes").findOne({token});
   if(!sessao){
-    return res.sendStatus(401)
+    return res.status(401).send("sessão invalida"); 
   }
   const { error } = transacaoSchema.validate(transacao, { abortEarly: false });
   if (error) {
@@ -155,14 +155,33 @@ app.post("/transactions",async(req,res)=>{
       return res.sendStatus(401); 
     }
     const sessao=await db.collection("sessoes").findOne({token});
-    console.log(sessao.userID);
+    
   if(!sessao){
     return res.sendStatus(401)
   }
-    try {
-      const transacoes = await db.collection("transacao").find({ userID: sessao.userID }).sort({ _id: -1 }).toArray();
-      if (transacoes.length > 0) {
-        return res.status(200).send(transacoes);
+  
+  const page = parseInt(req.query.page) || 1; 
+  var limit = parseInt(req.query.limit) || 10; 
+  if (limit>10){
+    limit=10;
+  }
+  if (limit <= 0) {
+    return res.sendStatus(400)
+  }
+
+  const skip = (page - 1) * limit;
+  try {
+    const transacoes = await db
+      .collection("transacao")
+      .find({ userID: sessao.userID })
+      .sort({ _id: -1 })
+      .skip(skip) 
+      .limit(limit) 
+      .toArray();
+
+    if (transacoes.length > 0) {
+      return res.status(200).send(transacoes);
+      
     }
        return res.status(404).send("nao existe nenhuma transação");
       
@@ -172,7 +191,59 @@ app.post("/transactions",async(req,res)=>{
     }
 });
 
+app.put("/transactions/:id", async (req, res) => {
+  const { id } = req.params;
+  const transacao = req.body;
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer", "").trim();
+  if(!token){
+    return res.sendStatus(401); 
+  }
+  const sessao=await db.collection("sessoes").findOne({token});
   
+  if(!sessao){
+  return res.sendStatus(401)
+  }
+
+  const { error } = transacaoSchema.validate(transacao, { abortEarly: false });
+  if (error) {
+      return res.status(422).send({
+          details: error.details.map((detail) => detail.message),
+      });
+  }
+    
+    
+    if (transacao.type.toUpperCase() !=="DEPOSIT" && transacao.type.toUpperCase() !== "WITHDRAW") {
+      return res.status(422).send("Tipo de transação invalida");
+    }  
+    if (!id) {
+      return res.status(400).send("ID da transação é obrigatório");
+    }
+    
+    try{
+      const transacaoAtualizada = {
+        valor: parseFloat(transacao.value).toFixed(2),
+        type:transacao.type,
+        description: transacao.description,
+        userID: sessao.userID,
+      };
+      const result = await db.collection("transacao").updateOne(
+        { _id: new ObjectId(id), userID: sessao.userID },
+        { $set: transacaoAtualizada }
+      );
+
+    if (result.matchedCount === 0) {
+      return res.status(404).send("Transação não encontrada");
+    }
+
+    return res.status(204).send("Transação atualizada com sucesso");
+    }catch (error) {
+      console.error(error);
+      return res.status(500).send("Erro ao atualizar a transação");
+    }
+})
+
+
 app.listen(process.env.PORT,()=>{
     console.log(`rodando liso na porta ${process.env.PORT}`)
 });
