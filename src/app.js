b7fdb406f709604f9f32d2fd532bb276dfdf80f4
Ananiasnt <netimcunha@gmail.com>
2024-12-18 21:28:49 -0300

    transactions com token de autenticação e criaçao da collection sessoe

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 import express, { json } from "express";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import cors from "cors";
 import Joi from "joi";
 import bcrypt from "bcrypt";
-import {v4 as uuid} from "uuid";
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv";
+dotenv.config();
 
 const app=express();
 app.use(json());
 app.use(cors());
 let db;
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+
 const userSchema= Joi.object({
     name:Joi.string().required(),
     email: Joi.string().email().required(),
@@ -19,7 +23,12 @@ const userLoginSchema= Joi.object({
   email: Joi.string().email().required(),
   password:Joi.string().required(),
 })
-const mongoClient=new MongoClient("mongodb://localhost:27017")
+const transacaoSchema= Joi.object({
+  value: Joi.number().required().positive(),
+  type:Joi.string().required(),
+  description:Joi.string().required()
+
+})
 
 mongoClient.connect()
   .then(() => {
@@ -81,7 +90,16 @@ mongoClient.connect()
       return res.status(401).send('Senha incorreta');
     }
     
-    const token = uuid(); 
+    const token = jwt.sign({},process.env.JWT_SECRET); 
+
+    const sessao={
+      token,
+      userID:emailCadastrado._id
+    };
+
+    await db.collection("sessoes").insertOne(sessao);
+    console.log(sessao);
+
     return res.status(200).send({ token });
   }catch{
     console.error(error);
@@ -90,10 +108,48 @@ mongoClient.connect()
   }
 
   })
+
+app.post("/transactions",async(req,res)=>{
+    const transacao=req.body;
+    const { authorization } = req.headers;
+    const token = authorization?.replace("Bearer", "").trim();
+    if(!token){
+      return res.sendStatus(401); 
+    }
+  const sessao=await db.collection("sessoes").findOne({token});
+  if(!sessao){
+    return res.sendStatus(401)
+  }
+  const { error } = transacaoSchema.validate(transacao, { abortEarly: false });
+  if (error) {
+      return res.status(422).send({
+          details: error.details.map((detail) => detail.message),
+      });
+  }
+    
+    
+    if (transacao.type.toUpperCase() !=="DEPOSIT" && transacao.type.toUpperCase() !== "WITHDRAW") {
+      return res.status(422).send("Tipo de transação invalida");
+    }  
+    try{
+       const newtransacao = {
+    valor: parseFloat(transacao.value).toFixed(2),
+    type: transacao.type,
+    description: transacao.description,
+    };
+
+    await db.collection("transicao").insertOne(newtransacao);
+    
+    res.status(201).send("transição adicionada com sucesso!" );
+    }
+   catch{
+    console.error(error);
+    res.status(500).send("Erro interno do servidor. Tente novamente mais tarde.");
+   }
+  })
  
 
   
-app.listen(5000,()=>{
-    console.log("rodando liso")
+app.listen(process.env.PORT,()=>{
+    console.log(`rodando liso na porta ${process.env.PORT}`)
 });
-
